4ae772043643bc030457aff2d6b77c2986e6f01e
AndrewQuezada <andrewevanq11@gmail.com>
2026-03-04 17:08:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,40 @@
-let alphabets = [
-    ['😀', '😆', '😢', '😆'],
-    ['😢', '😎', '😎', '🤔'],
-    ['😀', '😆', '😆', '🤔'],
-    ['😴', '😑', '😴', '😑'],
-];
-
-let firstCell = null;
-let firstRow = -1;
-let firstCol = -1;
-let matches = 0;
+//generating random number 1 - 100
+let randomNumber = Math.floor(Math.random() *100)+1;
 let attempts = 0;
-let lockBoard = false;
 
-function playGame() {
-    let html = "<table>";
-    for (let i = 0; i < alphabets.length; i++) {
-        html += "<tr>";
-        for (let j = 0; j < alphabets[i].length; j++) {
-            html += `<td onclick="cellClick(this, ${i}, ${j})">?</td>`;
-        }
-        html += "</tr>";
+//function
+function checkGuess(){
+    const guessInput = document.getElementById('guess');
+    const feedback = document.getElementById('feedback');
+    const userGuess = parseInt(guessInput.value);
+    //console.log(userGuess)
+
+    if(!userGuess|| userGuess < 1 || userGuess > 100){
+        feedback.textContent = "Please enter a number 1 from 100";
+        feedback.style.color="red";
+        return;
     }
-    html += "</table>";
-    document.getElementById("grid").innerHTML = html;
-    
-    firstCell = null;
-    firstRow = -1;
-    firstCol = -1;
-    matches = 0;
+
+    attempts++;
+    if(userGuess === randomNumber){
+        feedback.textContent=`congratulations! you guessed the number in ${attempts} attempts`;
+        feedback.style.color="red";
+    }
+    else if (userGuess > randomNumber){
+        feedback.textContent=`too high`;
+        feedback.style.color="red";
+    }
+    else{
+        feedback.textContent=`too low`;
+        feedback.style.color="red";
+    }
+    document.getElementById("guess").value = "";
+}
+
+function resetGame(){
+    randomNumber = Math.floor(Math.random()*100)+1;
     attempts = 0;
-    lockBoard = false;
-    displayStats();
-    document.getElementById("message").innerHTML = "Find the pairs!";
+    document.getElementById("guess").value = "";
+    document.getElementById("feedback").textContent = "";
 }
 
-function cellClick(cell, row, col) {
-    if (lockBoard || cell.innerHTML !== "?" || cell === firstCell) return;
-
-    cell.innerHTML = alphabets[row][col];
-
-    if (firstRow === -1) {
-        firstCell = cell;
-        firstRow = row;
-        firstCol = col;
-    } else {
-        attempts++;
-        if(alphabets[firstRow][firstCol] === alphabets[row][col]){
-matches++
-document.getElementById("message").innerHTML = "☑️ Match Found!";
-}else{
-    firstCell.innerHTML = "?";
-    cell.innerHTML = "?";
-    document.getElementById("message").innerHTML = "❌ Try Again"
-}
-    firstCell = null;
-    firstRow = -1;
-    firstCol = -1;
-    displayStats();
-    }
-}
-
-function displayStats() {
-    document.getElementById("stats").innerHTML = `Matches: ${matches} | Attempts: ${attempts}`;
-}
